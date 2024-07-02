c7a67352dc4305496594d4bcd4b31d546989f581
Rodrigo do Nascimento <rodrigoguigui@gmail.com>
2024-07-02 09:31:03 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/rnsolucoes/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.rnsolucoes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rnsolucoes.course.entities.Order;
+import com.rnsolucoes.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
