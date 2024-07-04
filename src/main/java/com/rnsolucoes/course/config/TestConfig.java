c84871a35e84757c4e8d18921526f944e8c7b795
Rodrigo do Nascimento <rodrigoguigui@gmail.com>
2024-07-04 18:03:21 -0300

    Payment, one-to-one association

--- src/main/java/com/rnsolucoes/course/config/TestConfig.java
+++ src/main/java/com/rnsolucoes/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rnsolucoes.course.entities.Category;
 import com.rnsolucoes.course.entities.Order;
 import com.rnsolucoes.course.entities.OrderItem;
+import com.rnsolucoes.course.entities.Payment;
 import com.rnsolucoes.course.entities.Product;
 import com.rnsolucoes.course.entities.User;
 import com.rnsolucoes.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2024-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
